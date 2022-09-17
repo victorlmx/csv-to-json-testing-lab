47f842325ba58dc9c3bb7ad8191cf39853d1f051
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-17 17:33:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-
-
 import csv
 import json
 
-with open('general.csv', newline='', mode="r") as csvFile:
-    csv_reader = csv.reader(csvFile, delimiter=',', quotechar='"', lineterminator="\n")
-    header = next(csv_reader)
-    #"Waypoint ID","Waypoint Date","Waypoint Time","X","Y","Comment","Last Modified","Last Modified By","Observation Category 0","Affiliation","Name Protected Area","Patrol Leader or Focal Point","Observation Group"
-    #print(header)
-    general = {}
-    for row in csv_reader:
-        observationGroup  = row[12]
-        #print(observationGroup)
-        if(observationGroup not in general.keys()):
-            general[observationGroup] = []
-        general[observationGroup].append([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]])
-        #print(general)
-        json_dump=json.dumps(general[observationGroup])
-        print(json_dump)
+def csv_to_json():
+    data_dict = {}
+    with open('general.csv', encoding = 'utf-8') as csv_file_handler:
+        csv_reader = csv.DictReader(csv_file_handler)
+        for rows in csv_reader:
+            key = rows['Observation Group']
+            data_dict[key] = rows
+    json_object=json.dumps(data_dict, indent = 4)
+    print(json_object)
+
+csv_to_json()
