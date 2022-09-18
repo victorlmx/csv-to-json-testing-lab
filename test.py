080ef783e3270cf6df8546f9a9b98595ffd5f424
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-17 21:24:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,40 @@
 import csv
 import json
 
-def csv_to_json():
+def dictionaryFromCSV(path):
     data_dict = {}
-    with open('general.csv', encoding = 'utf-8') as csv_file_handler:
+    with open(path, encoding = 'utf-8') as csv_file_handler:
+
         csv_reader = csv.DictReader(csv_file_handler)
         for rows in csv_reader:
-            key = rows['Observation Group']
-            data_dict[key] = rows
-    json_object=json.dumps(data_dict, indent = 4)
-    print(json_object)
+            dic_2={}
+            key = rows['Observation Group'] #make this the key value.
+            for item in rows.items():
+                #print(item[0])
+                if (item[0] != "Observation Group" and item[0] != "Observation Category 0"):
+                    dic_2[item[0]]=item[1]
+            data_dict[key]=dic_2
+    return data_dict
 
-csv_to_json()
+def setFromCSV(path):
+    data_set = []
+    with open(path, encoding = 'utf-8') as csv_file_handler:
+
+        csv_reader = csv.DictReader(csv_file_handler)
+        for row in csv_reader:
+            data_set.append(row)
+    return data_set
+
+general=dictionaryFromCSV('general.csv')
+sites=dictionaryFromCSV('site.csv')
+
+for observationGroup in sites:
+    if (observationGroup in general):
+        general[observationGroup]["Site Description"]= sites[observationGroup]
+    else:
+        print("XXXXXXX")
+
+print(general)
+exit()
+
+livestock=setFromCSV('livestock.csv')
