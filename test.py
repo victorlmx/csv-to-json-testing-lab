d00f12d0fe680bb784ff18d2d9428700a4bd0417
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-17 23:02:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,12 @@ import json
 def dictionaryFromCSV(path):
     data_dict = {}
     with open(path, encoding = 'utf-8') as csv_file_handler:
-
         csv_reader = csv.DictReader(csv_file_handler)
         for rows in csv_reader:
-            dic_2={}
             key = rows['Observation Group'] #make this the key value.
-            for item in rows.items():
-                #print(item[0])
-                if (item[0] != "Observation Group" and item[0] != "Observation Category 0"):
-                    dic_2[item[0]]=item[1]
-            data_dict[key]=dic_2
+            #rows.pop('Observation Group')
+            #rows.pop('Observation Category 0')
+            data_dict[key]=rows
     return data_dict
 
 def setFromCSV(path):
@@ -25,16 +21,41 @@ def setFromCSV(path):
             data_set.append(row)
     return data_set
 
+
 general=dictionaryFromCSV('general.csv')
 sites=dictionaryFromCSV('site.csv')
-
-for observationGroup in sites:
-    if (observationGroup in general):
-        general[observationGroup]["Site Description"]= sites[observationGroup]
-    else:
-        print("XXXXXXX")
-
-print(general)
-exit()
-
 livestock=setFromCSV('livestock.csv')
+output = {}
+
+#note: item is a dict
+for item in livestock:
+    key = item["Observation Group"]
+    if (key not in output):
+        output[item["Observation Group"]]=[item["Observation Category 0"],[]]
+    item.pop("Observation Category 0")
+    item.pop("Observation Group")
+    output[key][1].append(item)
+
+print(json.dumps(output))
+
+#print(livestock)
+
+#for item in general.items():
+
+
+#output = {}
+#dict1 = {}
+#dict2 = {}
+
+#output["a"]={}
+#print(output)
+#dict1["General"]={}
+#dict2["Site"]={}
+
+#output["a"]=dict1
+#print(output)
+#output["a"].update(dict2)
+
+#print(output)
+
+exit()
