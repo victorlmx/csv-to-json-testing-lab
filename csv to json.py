c39470e863c1ac0d8cd76248356015c9992574b3
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-21 18:59:50 -0500

    small refactor

--- csv to json.py
+++ csv to json.py
@@ -23,52 +23,44 @@ wildlife=setFromCSV('wildlife.csv')
 animal=setFromCSV('animal.csv')
 dictionary_output={}
 
-
-def buildDic(name, list):
+def buildDictionary(name, list, idIdentifier=None, category=None):
     global dictionary_output
+    if (name == str_livestock or name ==str_wildlife):
+        sub_category = {}
+        n = 1
+        map_temp = {}
     for item in list:
         key = item["Observation Group"]
         item.pop("Observation Group")
         item.pop("Observation Category 0")
+
+        if (name == str_livestock or name ==str_wildlife):
+            sub_category_name = ""
+            sub_category_id = item[idIdentifier]
+            dict2_key2 = item["Animals Observed per Species"]
+            item.pop("Animals Observed per Species")
+            if (sub_category_id not in map_temp): 
+                map_temp[sub_category_id] = n
+                sub_category_name = category + " " + str(n)
+                sub_category[sub_category_name] = {"Adult Male": [],"Adult Female": [],"Adult Unknown Sex": [],"Juvenile": [],"Fetus": [], "Unknown Sex and Age": []}
+                n += 1
+            else:
+                sub_category_name = category + " " + str(map_temp[sub_category_id])
+
         if (key not in dictionary_output): 
             dictionary_output[key] = {str_general: [],str_site: [],str_wildlife: [],str_livestock: [],str_animals: []}
         dictionary_output[key][name].append(item)
-
-def buildWildDic(name, list):
-    global dictionary_output
-    dict2 = {}
-    n = 1
-    dict3_temp = {}
-    for item in list:
-        key = item["Observation Group"]
-        dict2_key = ""
-        item.pop("Observation Group")
-        item.pop("Observation Category 0")
-        if (key not in dictionary_output): 
-            dictionary_output[key] = {str_general: [],str_site: [],str_wildlife: [],str_livestock: [],str_animals: []}
-        widlifeID = item["Provide Species not Listed"]
-        dict2_key2 = item["Animals Observed per Species"]
-        item.pop("Animals Observed per Species")
-        if (widlifeID not in dict3_temp): 
-            dict3_temp[widlifeID] = n
-            dict2_key = "Wildlife " + str(n)
-            dict2[dict2_key] = {"Adult Male": [],"Adult Female": [],"Adult Unknown Sex": [],"Juvenile": [],"Fetus": [], "Unknown Sex and Age": []}
-            n += 1
-        else:
-            dict2_key = "Wildlife " + str(dict3_temp[widlifeID])
-        dict2[dict2_key][dict2_key2].append(item)
-    dictionary_output[key][name].append(dict2)
 
 def deleteEmpty(key, name):
     global dictionary_output
     if (len(dictionary_output[key][name])==0):
         dictionary_output[key].pop(name)
 
-buildDic(str_general, general)
-buildDic(str_site, sites)
-buildWildDic(str_wildlife, wildlife)
-buildDic(str_animals, animal)
-buildDic(str_livestock, livestock)
+buildDictionary(str_general, general)
+buildDictionary(str_site, sites)
+buildDictionary(str_wildlife, wildlife, "Provide Species not Listed", "Wildlife")
+buildDictionary(str_animals, animal)
+buildDictionary(str_livestock, livestock, "Other Relevant Information", "Livestock")
 
 for key in dictionary_output:
     deleteEmpty(key, str_general)
