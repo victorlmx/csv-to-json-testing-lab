e73b2550a6aeec4574df3fb34b75e0ae8d191584
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-21 19:32:21 -0500

    Update csv to json.py

--- csv to json.py
+++ csv to json.py
@@ -23,33 +23,39 @@ wildlife=setFromCSV('wildlife.csv')
 animal=setFromCSV('animal.csv')
 dictionary_output={}
 
-def buildDictionary(name, list, idIdentifier=None, category=None):
+def buildDictionary(name, list, sub_category_id_column=None, category=None):
     global dictionary_output
-    if (name == str_livestock or name ==str_wildlife):
+    if (name == str_livestock or name ==str_wildlife or name ==str_animals):
         sub_category = {}
         n = 1
         map_temp = {}
     for item in list:
         key = item["Observation Group"]
+        if (key not in dictionary_output): 
+            dictionary_output[key] = {str_general: [],str_site: [],str_wildlife: [],str_livestock: [],str_animals: []}   
         item.pop("Observation Group")
         item.pop("Observation Category 0")
 
-        if (name == str_livestock or name ==str_wildlife):
-            sub_category_name = ""
-            sub_category_id = item[idIdentifier]
-            dict2_key2 = item["Animals Observed per Species"]
+        if (name == str_general or name ==str_site):
+            dictionary_output[key][name].append(item)
+        elif (name == str_livestock or name ==str_wildlife):
+            sub_category_key = ""
+            sub_category_id = item[sub_category_id_column]
+            animal_per_species = item["Animals Observed per Species"] #e.g., "Adult Male", "Adult Female", etc.
             item.pop("Animals Observed per Species")
             if (sub_category_id not in map_temp): 
                 map_temp[sub_category_id] = n
-                sub_category_name = category + " " + str(n)
-                sub_category[sub_category_name] = {"Adult Male": [],"Adult Female": [],"Adult Unknown Sex": [],"Juvenile": [],"Fetus": [], "Unknown Sex and Age": []}
+                sub_category_key = category + " " + str(n)
+                sub_category[sub_category_key] = {"Adult Male": [],"Adult Female": [],"Adult Unknown Sex": [],"Juvenile": [],"Fetus": [], "Unknown Sex and Age": []}
                 n += 1
             else:
-                sub_category_name = category + " " + str(map_temp[sub_category_id])
-
-        if (key not in dictionary_output): 
-            dictionary_output[key] = {str_general: [],str_site: [],str_wildlife: [],str_livestock: [],str_animals: []}
-        dictionary_output[key][name].append(item)
+                sub_category_key = category + " " + str(map_temp[sub_category_id])
+            sub_category[sub_category_key][animal_per_species].append(item)
+        elif (name == str_animals):
+            print("To-Do")
+    if (name == str_livestock or name ==str_wildlife):
+        dictionary_output[key][name].append(sub_category)
+        
 
 def deleteEmpty(key, name):
     global dictionary_output
@@ -59,7 +65,7 @@ def deleteEmpty(key, name):
 buildDictionary(str_general, general)
 buildDictionary(str_site, sites)
 buildDictionary(str_wildlife, wildlife, "Provide Species not Listed", "Wildlife")
-buildDictionary(str_animals, animal)
+buildDictionary(str_animals, animal, "Animal ID", "Animal")
 buildDictionary(str_livestock, livestock, "Other Relevant Information", "Livestock")
 
 for key in dictionary_output:
