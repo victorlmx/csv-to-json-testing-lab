8fe87c7bfc5ebe5c8929052df8c7dd331371f898
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-23 18:46:44 -0500

    Update csv to json.py

--- csv to json.py
+++ csv to json.py
@@ -34,7 +34,6 @@ wildlife=[]
 animal=[]
 
 observations={}
-animals_observed_per_species={}
 
 def readCSVs():
     global general, sites, livestock, wildlife, animal
@@ -73,11 +72,19 @@ def addCategoryInfo(id, category_name, list):
     print("-- processing observation", id, ", waypoint", list["Waypoint ID"])
     observations[id][category_name]=list
 
+
+
 def buildCategoryStructure(category_name, list, sub_category_id_column=None):
+    def buildListCategories(id):
+        if (id not in listCategoryNames): 
+            listCategoryNames[id] = len(listCategoryNames)+1
+            sub_categories[id] = {}   
+
+    sub_categories = {} #subcategories, names and items.
+    listCategoryNames = {} #names only, e.g., Species wildlife 1 event 1
+    animals_observed_per_species = {} #information about juvenile healthy, sick, dead, etc.
+    
     print("Building json for ", category_name, ":")
-    sub_category = {} #e.g., Wildlife 1, Wildlife 2,...
-    sub_category_items = {}
-    listCategories = {}  #e.g., cat=1, boar=2
 
     for item in list:
         observation_group = observationGroup(item)
@@ -86,16 +93,14 @@ def buildCategoryStructure(category_name, list, sub_category_id_column=None):
         
         if (category_name == str_livestock or category_name ==str_wildlife or category_name == str_animals):
             print("-- processing observation", observation_group, ", ID:", item[sub_category_id_column])
-            
-            sub_category_id = item[sub_category_id_column]
+            animals_observed_per_species_item={}
 
-            if (sub_category_id not in listCategories): 
-                listCategories[sub_category_id] = len(listCategories)+1
-                sub_category[sub_category_id] = {}
+            sub_category_id = item[sub_category_id_column]
+            buildListCategories(sub_category_id)
 
         if (category_name == str_animals):
-            sub_category[sub_category_id]=item
-            observations[observation_group][category_name]=sub_category
+            sub_categories[sub_category_id]=item
+            observations[observation_group][category_name]=sub_categories
 
         if (category_name == str_livestock or category_name ==str_wildlife):
             #REQ: Group Wildlife and Livestock items by "Animals Observed per Species"
@@ -107,27 +112,27 @@ def buildCategoryStructure(category_name, list, sub_category_id_column=None):
             #},
             animal_per_species = item["Animals Observed per Species"] #e.g., "Adult Male", "Adult Female", etc.
             item.pop("Animals Observed per Species")
-            animals_observed_per_species[animal_per_species + " Healthy"]= item["Number Healthy"]
-            animals_observed_per_species[animal_per_species + " Sick or Injured"]= item["Number Sick or Injured"]
-            animals_observed_per_species[animal_per_species + " Dead"]= item["Number Dead"]
+            animals_observed_per_species_item[animal_per_species + " Healthy"]= item["Number Healthy"]
+            animals_observed_per_species_item[animal_per_species + " Sick or Injured"]= item["Number Sick or Injured"]
+            animals_observed_per_species_item[animal_per_species + " Dead"]= item["Number Dead"]
             item.pop("Number Dead")
             item.pop("Number Healthy")
             item.pop("Number Sick or Injured")
 
             #append animals_observed_per_species to the list that will ultimately be assigned to the subcategory (e.g., "Widlife 1" or "Wildlife 2") outside of the for loop.
-            if listCategories[sub_category_id] in sub_category_items:
-                sub_category_items[listCategories[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species)
+            if listCategoryNames[sub_category_id] in animals_observed_per_species:
+                animals_observed_per_species[listCategoryNames[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species_item)
             else:
-                sub_category_items[listCategories[sub_category_id]]={"Animals Observed Per Species":[]}
-                sub_category_items[listCategories[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species)
+                animals_observed_per_species[listCategoryNames[sub_category_id]]={"Animals Observed Per Species":[]}
+                animals_observed_per_species[listCategoryNames[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species_item)
             
-            sub_category[sub_category_id]=item
+            sub_categories[sub_category_id]=item
 
     #add the multiple animals_observed_per_species to the subcategory.
     if (category_name == str_livestock or category_name ==str_wildlife):
-        for idx, value in enumerate(sub_category.values()):
-            value["Animals Observed Per Species"]=sub_category_items[idx+1]["Animals Observed Per Species"]
-        observations[observation_group][category_name]=sub_category
+        for idx, value in enumerate(sub_categories.values()):
+            value["Animals Observed Per Species"]=animals_observed_per_species[idx+1]["Animals Observed Per Species"]
+        observations[observation_group][category_name]=sub_categories
 
 readCSVs()
 buildStructure()
