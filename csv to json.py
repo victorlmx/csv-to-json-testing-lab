5b7d54125a2281f92064c78aa58854d2e2677c31
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-23 00:17:28 -0500

    Update csv to json.py

--- csv to json.py
+++ csv to json.py
@@ -4,6 +4,7 @@
 
 import csv
 import json
+from collections import defaultdict
 
 def setFromCSV(path):
     data_set = []
@@ -12,7 +13,6 @@ def setFromCSV(path):
         for row in csv_reader:
             data_set.append(row)
     return data_set
-
 
 str_animals= "Animal and Samples"
 str_livestock = "Livestock - Domestic Species"
@@ -32,9 +32,10 @@ def buildDictionary(category_name, list, sub_category_id_column=None, category=N
     global observation_group_dic
     if (category_name == str_livestock or category_name ==str_wildlife or category_name ==str_animals):
         sub_category = {} #e.g., Wildlife 1, Wildlife 2,...
-        a={} #e.g., animals observed per species 
+        sub_category_items = {}
+
         n = 1
-        map_temp = {}  #e.g., cat=1, boar=2
+        map_categories = {}  #e.g., cat=1, boar=2
     for item in list:
         #REQ: The 1st level is the Observation Group ID.
         observation_group = item["Observation Group"]
@@ -45,37 +46,48 @@ def buildDictionary(category_name, list, sub_category_id_column=None, category=N
         item.pop("Observation Category 0")
 
         #REQ: General info and Site Description doesn't have any nested items.
-        if (category_name == str_general or category_name ==str_site):
+        if (category_name == str_general or category_name ==str_site or category_name == str_animals):
             observation_group_dic[observation_group][category_name]=item
         
         #REQ: Livestock and Wildlife have nested items (e.g., "Wildlife 1", "Wildlife 2")
         elif (category_name == str_livestock or category_name ==str_wildlife):
+            animals_observed_per_species={}
             sub_category_key = ""
             sub_category_id = item[sub_category_id_column]
 
             #REQ: Group Wildlife and Livestock items by "Animals Observed per Species"
             animal_per_species = item["Animals Observed per Species"] #e.g., "Adult Male", "Adult Female", etc.
             item.pop("Animals Observed per Species")
-            if (sub_category_id not in map_temp): 
-                map_temp[sub_category_id] = n
+            if (sub_category_id not in map_categories): 
+                map_categories[sub_category_id] = n
                 sub_category_key = category + " " + str(n)
                 sub_category[sub_category_key] = {}
                 n += 1
             else:
-                sub_category_key = category + " " + str(map_temp[sub_category_id])
+                sub_category_key = category + " " + str(map_categories[sub_category_id])
 
-            a[animal_per_species + " Healthy"]= item["Number Healthy"]
-            a[animal_per_species + " Sick or Injured"]= item["Number Sick or Injured"]
-            a[animal_per_species + " Dead"]= item["Number Dead"]
-            item["animals observed per species"]=a
+            animals_observed_per_species[animal_per_species + " Healthy"]= item["Number Healthy"]
+            animals_observed_per_species[animal_per_species + " Sick or Injured"]= item["Number Sick or Injured"]
+            animals_observed_per_species[animal_per_species + " Dead"]= item["Number Dead"]
+            item.pop("Number Dead")
+            item.pop("Number Healthy")
+            item.pop("Number Sick or Injured")
+
+            if map_categories[sub_category_id] in sub_category_items:
+                sub_category_items[map_categories[sub_category_id]]["animals observed per species"].append(animals_observed_per_species)
+            else:
+                sub_category_items[map_categories[sub_category_id]]={"animals observed per species":[]}
+                sub_category_items[map_categories[sub_category_id]]["animals observed per species"].append(animals_observed_per_species)
+            
             sub_category[sub_category_key]=item
-        elif (category_name == str_animals):
-            print("to do")
-    if (category_name == str_livestock or category_name ==str_wildlife):
-        
-        observation_group_dic[observation_group][category_name]=sub_category
-        
 
+    if (category_name == str_livestock or category_name ==str_wildlife):
+        n=1
+        for value in sub_category.values():
+            value["animals observed per species"]=sub_category_items[n]["animals observed per species"]
+            n += 1
+        observation_group_dic[observation_group][category_name]=sub_category
+    
 def deleteEmpty(key, name):
     global observation_group_dic
     if (len(observation_group_dic[key][name])==0):
@@ -85,7 +97,7 @@ def deleteEmpty(key, name):
 buildDictionary(str_general, general)
 buildDictionary(str_site, sites)
 buildDictionary(str_wildlife, wildlife, "Provide Species not Listed", "Wildlife")
-buildDictionary(str_animals, animal, "Animal ID", "Animal")
+buildDictionary(str_animals, animal)
 buildDictionary(str_livestock, livestock, "Other Relevant Information", "Livestock")
 
 #REQ: Create JSON schema.
