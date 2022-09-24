742602ed077365a9efcaf571bd29de12d4104c25
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-09-23 20:42:09 -0500

    Update csv to json.py

--- csv to json.py
+++ csv to json.py
@@ -4,35 +4,22 @@
 
 #note try &includeuuids=true
 
-
-
-
-
-
 #TO-DO: Generate Animal Sample Records
-
-
-
-
-
 
 import csv
 import json
 from collections import defaultdict
-
 
 str_animals= "Animal and Samples"
 str_livestock = "Livestock - Domestic Species"
 str_wildlife = "Wildlife"
 str_site = "Site Description"
 str_general = "General Information"
-
 general=[]
 sites=[]
 livestock=[]
 wildlife=[]
 animal=[]
-
 observations={}
 
 def readCSVs():
@@ -72,50 +59,29 @@ def addCategoryInfo(id, category_name, list):
     print("-- processing observation", id, ", waypoint", list["Waypoint ID"])
     observations[id][category_name]=list
 
-def addSubCategoryInfo(observationGroup, category_name, item_id, items ): ######################
+def addSubCategoryToObservation(observationGroup, category_name, item_id, items ): ######################
     if (category_name in observations[observationGroup]):
         observations[observationGroup][category_name][item_id]=items
     else: 
         observations[observationGroup][category_name] = {item_id: items}
 
 def buildCategoryStructure(category_name, list, sub_category_id_column=None):
-    def buildListCategories(id):
-        if (id not in listCategoryNames): 
-            listCategoryNames[id] = len(listCategoryNames)+1
-            sub_categories[id] = {}   
-
+    print("Building json for ", category_name, ":")
     sub_categories = {} #subcategories, names and items.
     listCategoryNames = {} #names only, e.g., Species wildlife 1 event 1
     animals_observed_per_species = {} #information about juvenile healthy, sick, dead, etc.
     boolWildlifeOrLivestock = category_name == str_livestock or category_name ==str_wildlife 
 
+    def buildListCategories(id):
+        if (id not in listCategoryNames): 
+            listCategoryNames[id] = len(listCategoryNames)+1
+            sub_categories[id] = {}  
 
-    print("Building json for ", category_name, ":")
-
-    for item in list:
-        observation_group = observationGroup(item)
-        if (category_name == str_general or category_name ==str_site):
-            addCategoryInfo(observation_group,category_name, item)
-        
-        if (boolWildlifeOrLivestock or category_name == str_animals):
-            print("-- processing observation", observation_group, ", ID:", item[sub_category_id_column])
-            animals_observed_per_species_item={}
-
-            sub_category_id = item[sub_category_id_column]
-            buildListCategories(sub_category_id)
-
+    def addSubCategoryInfo():
+        animals_observed_per_species_item={}
         if (category_name == str_animals):
             sub_categories[sub_category_id]=item
-            #addSubCategoryInfo(observation_group, category_name, sub_categories)
-
         if (boolWildlifeOrLivestock):
-            #REQ: Group Wildlife and Livestock items by "Animals Observed per Species"
-            #Generate something like 
-            #{
-            #    "Adult Female Healthy":"5.0",
-            #    "Adult Female Sick or Injured":"0.0",
-            #    "Adult Female Dead":"1.0"
-            #},
             animal_per_species = item["Animals Observed per Species"] #e.g., "Adult Male", "Adult Female", etc.
             item.pop("Animals Observed per Species")
             animals_observed_per_species_item[animal_per_species + " Healthy"]= item["Number Healthy"]
@@ -124,25 +90,36 @@ def buildCategoryStructure(category_name, list, sub_category_id_column=None):
             item.pop("Number Dead")
             item.pop("Number Healthy")
             item.pop("Number Sick or Injured")
-
             #append animals_observed_per_species to the list that will ultimately be assigned to the subcategory (e.g., "Widlife 1" or "Wildlife 2") outside of the for loop.
             if listCategoryNames[sub_category_id] in animals_observed_per_species:
                 animals_observed_per_species[listCategoryNames[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species_item)
             else:
                 animals_observed_per_species[listCategoryNames[sub_category_id]]={"Animals Observed Per Species":[]}
                 animals_observed_per_species[listCategoryNames[sub_category_id]]["Animals Observed Per Species"].append(animals_observed_per_species_item)
-            
             sub_categories[sub_category_id]=item
 
-    #add the multiple animals_observed_per_species to the subcategory.
-    if (boolWildlifeOrLivestock or category_name == str_animals):    
-        n = 0    
-        for idx, sub_category in sub_categories.items():
-            n += 1
-            if (boolWildlifeOrLivestock):
-                sub_category["Animals Observed Per Species"]=animals_observed_per_species[n]["Animals Observed Per Species"]
-            addSubCategoryInfo(sub_category["Observation Group"], category_name, idx, sub_category)
+    def addSubCategoryToObservations():
+        if (boolWildlifeOrLivestock or category_name == str_animals):    
+            n = 0    
+            for idx, sub_category in sub_categories.items():
+                n += 1
+                if (boolWildlifeOrLivestock):
+                    sub_category["Animals Observed Per Species"]=animals_observed_per_species[n]["Animals Observed Per Species"]
+                addSubCategoryToObservation(sub_category["Observation Group"], category_name, idx, sub_category)
 
+    for item in list:
+        observation_group = observationGroup(item)
+        if (category_name == str_general or category_name ==str_site):
+            addCategoryInfo(observation_group,category_name, item)
+
+        if (boolWildlifeOrLivestock or category_name == str_animals):
+            print("-- processing observation", observation_group, ", ID:", item[sub_category_id_column])
+            sub_category_id = item[sub_category_id_column]
+            buildListCategories(sub_category_id)
+            addSubCategoryInfo()
+    
+    addSubCategoryToObservations()
+    
 readCSVs()
 buildStructure()
 print("==================Complete JSON Below==================")
